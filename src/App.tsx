f9a717bdcc66ea0b3fea5fdb96b28436e89c79b8
Samuel Deotti <samuel_deotti@hotmail.com>
2024-04-02 18:50:29 -0300

    adding Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
-import Initial from './pages/Initial';
-import './style.css';
-
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Initial /> } />
-    </Routes>
+    <div id="content">
+      <header
+        style={ {
+          backgroundColor: 'white',
+          width: '100%',
+          height: '60px',
+          display: 'flex',
+          alignItems: 'center',
+          gap: '30px',
+          boxShadow: '0px 0px 12px 0px rgba(0,0,0,0.3)',
+        } }
+      >
+        <h1
+          style={ { marginLeft: '30px', height: '100%', display: 'flex', alignItems: 'center' } }
+        >
+          <img
+            src="/beLogo.png"
+            alt="logo bemobile"
+            style={ { height: '35px', width: '44px' } }
+          />
+        </h1>
+      </header>
+
+    </div>
   );
 }
 
--- src/pages/Initial.tsx
+++
@@ -1,9 +0,0 @@
-function Initial() {
-  return (
-    <div>
-      <p>inicio</p>
-    </div>
-  );
-}
-
-export default Initial;
